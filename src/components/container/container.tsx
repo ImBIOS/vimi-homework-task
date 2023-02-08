1a2b249a75b3f48b15a2d6c5c22a49010f3682b0
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-02-08 17:15:12 +0700

    chore: move file component to folder

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Container = ({ children }: Props) => {
   return (
-    <main className="bg-gradient-to-tr from-indigo-200 via-red-200 to-yellow-100 min-h-screen">
+    <main className="bg-gradient-to-tr from-indigo-200 via-red-200 to-yellow-100 min-h-screen p-4">
       {children}
     </main>
   );
