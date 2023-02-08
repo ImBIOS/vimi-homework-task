b1e5377c751d8c4eb4a2528ba5a4f3273908c06a
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-02-08 14:11:56 +0700

    style: implement tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from "./components/container";
+import Title from "./components/title";
+import data from "./data/db";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container>
+      <header className="mx-8 mb-4">
+        <Title>
+          <b>Hello</b> Gerard.
+        </Title>
+        <p>Here are the list of projects you submitted.</p>
       </header>
-    </div>
+      <section className="px-4 py-8">
+        <section className="flex justify-between">
+          <Title>
+            <b>Recent Projects</b>
+          </Title>
+          <div>Sort by v</div>
+        </section>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Status</th>
+              <th>Created</th>
+              <th>Manage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Project 1</td>
+              <td>Project Type 1</td>
+              <td>Project Status 1</td>
+              <td>Project Date 1</td>
+              <td>Manage</td>
+            </tr>
+            {data.map((project) => (
+              <tr key={project.id}>
+                <td>{project.name}</td>
+                <td>{project.type}</td>
+                <td>{project.status}</td>
+                <td>{project.createdOn}</td>
+                <td>Manage</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </Container>
   );
 }
 
